ca8bf6aea894405942ac5450cb7893312203a1e8
Manlio Morini <morinim@gmail.com>
2023-10-03 12:31:58 +0200

    <test>: complete test coverage for value_t type

--- src/test/value.cc
+++ src/test/value.cc
@@ -13,6 +13,7 @@
 #include <sstream>
 
 #include "kernel/value.h"
+#include "utility/misc.h"
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "third_party/doctest/doctest.h"
@@ -25,46 +26,82 @@ TEST_CASE("Base")
   using namespace ultra;
 
   value_t v1;
-  CHECK(!has_value(v1));
-  CHECK(v1.index() == d_void);
-  const bool self_eq(v1 == v1);
-  CHECK(self_eq);
+  std::stringstream out;
 
-  v1 = std::string("dummy");
-  CHECK(has_value(v1));
-  CHECK(v1.index() == d_string);
-  const bool string_eq(v1 == v1);
-  CHECK(string_eq);
+  SUBCASE("Empty value")
+  {
+    CHECK(!has_value(v1));
+    CHECK(v1.index() == d_void);
+    const bool self_eq(v1 == v1);
+    CHECK(self_eq);
 
-  v1 = [] { std::cout << "Hello world\n"; };
-  CHECK(has_value(v1));
-  CHECK(v1.index() == d_nullary);
-  const bool nullary_eq(v1 == v1);
-  CHECK(nullary_eq);
+    out << v1;
+    CHECK(out.str() == "[]");
+  }
 
-  v1 = 1;
-  CHECK(has_value(v1));
-  CHECK(v1.index() == d_int);
-  const bool int_eq(v1 == v1);
-  CHECK(int_eq);
+  SUBCASE("String value")
+  {
+    v1 = std::string("dummy");
+    CHECK(has_value(v1));
+    CHECK(v1.index() == d_string);
+    const bool string_eq(v1 == v1);
+    CHECK(string_eq);
 
-  v1 = 1.0;
-  CHECK(has_value(v1));
-  CHECK(v1.index() == d_double);
-  const bool double_eq(v1 == v1);
-  CHECK(double_eq);
+    out << v1;
+    CHECK(out.str() == std::get<D_STRING>(v1));
+  }
 
-  value_t v2(1.00000000000001);
-  CHECK(has_value(v2));
-  CHECK(v2.index() == d_double);
-  const bool almost_eq(v1 == v2);
-  CHECK(almost_eq);
+  SUBCASE("Nullary value")
+  {
+    v1 = [] { std::cout << "Hello world\n"; };
+    CHECK(has_value(v1));
+    CHECK(v1.index() == d_nullary);
+    const bool nullary_eq(v1 == v1);
+    CHECK(nullary_eq);
 
-  v1 = 1;
-  CHECK(has_value(v1));
-  CHECK(v1.index() == d_int);
-  const bool type_diff(v1 != v2);
-  CHECK(type_diff);
+    out << v1;
+    CHECK(out.str() == "[]()");
+  }
+
+  SUBCASE("Integer value")
+  {
+    v1 = 1;
+    CHECK(has_value(v1));
+    CHECK(v1.index() == d_int);
+    const bool int_eq(v1 == v1);
+    CHECK(int_eq);
+
+    out << v1;
+    CHECK(out.str() == std::to_string(std::get<D_INT>(v1)));
+  }
+
+  SUBCASE("Double value")
+  {
+    v1 = 1.0;
+    CHECK(has_value(v1));
+    CHECK(v1.index() == d_double);
+    const bool double_eq(v1 == v1);
+    CHECK(double_eq);
+
+    out << v1;
+    CHECK(almost_equal(std::stod(out.str()), std::get<D_DOUBLE>(v1)));
+
+    value_t v2(1.00000000000001);
+    CHECK(has_value(v2));
+    CHECK(v2.index() == d_double);
+    const bool almost_eq(v1 == v2);
+    CHECK(almost_eq);
+  }
+
+  SUBCASE("Different types comparison")
+  {
+    value_t v2(1.0);
+    v1 = 1;
+    CHECK(has_value(v1));
+    CHECK(v1.index() == d_int);
+    const bool type_diff(v1 != v2);
+    CHECK(type_diff);
+  }
 }
 
 }  // TEST_SUITE("VALUE_T")
