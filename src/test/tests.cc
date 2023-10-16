49ca5db79311e57b8e9c83356c04da8446016c56
Manlio Morini <morinim@gmail.com>
2023-10-16 16:45:37 +0200

    test: add overall test suite

---
+++ src/test/tests.cc
@@ -0,0 +1,15 @@
+/**
+ *  \file
+ *  \remark This file is part of ULTRA.
+ *
+ *  \copyright Copyright (C) 2023 EOS di Manlio Morini.
+ *
+ *  \license
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ *  You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+
+#include "test/function.cc"
+#include "test/terminal.cc"
+#include "test/value.cc"
