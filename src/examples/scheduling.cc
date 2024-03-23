ea4cdcfe3bcbd46dd2ba787fe4e14abca7688396
Manlio Morini <morinim@gmail.com>
2024-03-23 18:36:39 +0100

    feat: add scheduling example

---
+++ src/examples/scheduling.cc
@@ -0,0 +1,106 @@
+/*
+ *  \remark This file is part of ULTRA.
+ *
+ *  \copyright Copyright (C) 2024 EOS di Manlio Morini.
+ *
+ *  \license
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ *  You can obtain one at http://mozilla.org/MPL/2.0/
+ *
+ *  \see https://github.com/morinim/vita/wiki/scheduling_tutorial
+ */
+
+#include <chrono>
+#include <vector>
+
+#include "kernel/ultra.h"
+
+// Examples taken from "Differential Evolution in Discrete Optimization" by
+// Daniel Lichtblau.
+
+//`n_machines` homogeneous machines (i.e. each job time is independent of the
+// machine used.
+const int n_machines = 5;
+
+// `n_jobs` with random durations.
+const int n_jobs = 50;
+std::vector<std::chrono::hours> job_duration(n_jobs);
+
+// Assuming a total time period of one day.
+double f(ultra::de::individual start)
+{
+  double ret(0.0);
+
+  for (auto &s : start)
+    s = std::round(s);
+
+  for (unsigned i(0); i < start.parameters(); ++i)
+  {
+    // A job starts at a nonnegative time.
+    if (start[i] < 0.0)
+      ret += start[i];
+
+    // A job starts more than its length prior to the 24 hour limit.
+    const auto end(start[i] + job_duration[i].count());
+    if (end >= 24.0)
+      ret -= end - 24.0;
+
+    int occupied(1);
+    for (unsigned j(0); j < start.parameters(); ++j)
+      if (j != i
+          && start[j] <= start[i]
+          && start[j] + job_duration[j].count() > start[i])
+        ++occupied;
+
+    if (occupied > n_machines)
+      ret -= occupied - n_machines;
+  }
+
+  return ret;
+}
+
+int main()
+{
+  using namespace ultra;
+  using namespace std::chrono_literals;
+
+  std::ranges::generate(job_duration,
+                        []
+                        {
+                          return std::chrono::hours(random::between(1, 4));
+                        });
+
+  std::cout << "Total time required: "
+            << std::accumulate(job_duration.begin(), job_duration.end(),
+                               0h).count()
+            << '\n';
+
+  // A candidate solution is a sequence of `n_jobs` doubles in the
+  // `[-0.5, 23.5[` interval.
+  de::problem prob(n_jobs, {-0.5, 23.5});
+
+  prob.params.population.individuals =  250;
+  prob.params.evolution.generations  = 2000;
+  //prob.params.evolution.tournament_size   = 40;
+
+  prob.params.stat.dynamic_file = "dyn.txt";
+  prob.params.stat.population_file = "pop.txt";
+
+  de::search src(prob, f);
+
+  const auto res(src.run(10).best_individual);
+
+  for (unsigned i(0); i < n_jobs; ++i)
+    std::cout << i << ' ' << std::round(res[i]) << ' '
+              << job_duration[i].count() << '\n';
+
+  std::cout << "Fitness: " << f(res) << std::endl;
+
+  // A simple script for GnuPlot:
+  // set xtics 1
+  // set ytics 2
+  // grid xtics ytics
+  // plot [x=0:24][y=-0.5:50.5] "test.dat" using 2:1:3:(0)
+  //      w vectors head filled lw 2 notitle
+}
---
+++ src/kernel/ultra.h
@@ -0,0 +1,29 @@
+/**
+ *  \file
+ *  \remark This file is part of ULTRA.
+ *
+ *  \copyright Copyright (C) 2024 EOS di Manlio Morini.
+ *
+ *  \license
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ *  You can obtain one at http://mozilla.org/MPL/2.0/
+ *
+ *  Convenience header composed to be used by Ultra framework's clients.
+ *  Ultra isn't Boost and a master header is a simplification for the API user.
+ *  Anyway to reduce compile times in large projects, clients can include only
+ *  individual headers.
+ *
+ *  \warning **NOT TO BE USED INTERNALLY**
+ */
+
+#if !defined(ULTRA_ULTRA_H)
+#define      ULTRA_ULTRA_H
+
+#include "kernel/de/individual.h"
+#include "kernel/de/primitive.h"
+#include "kernel/de/problem.h"
+#include "kernel/de/search.h"
+#include "kernel/gp/individual.h"
+
+#endif  // include guard
