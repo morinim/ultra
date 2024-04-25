c37cf88cbc5f72f430b2f9b49b31704d97933c43
Manlio Morini <morinim@gmail.com>
2024-04-25 17:51:32 +0200

    test: replace `sset.insert` calls with `prob.insert`

--- src/test/fixture2.h
+++ src/test/fixture2.h
@@ -40,38 +40,38 @@ struct fixture2
 
   ultra::problem prob {};
 
-  ultra::real::literal *c0 {prob.sset.insert<ultra::real::literal>(0.0)};
-  ultra::real::literal *c1 {prob.sset.insert<ultra::real::literal>(1.0)};
-  ultra::real::literal *c2 {prob.sset.insert<ultra::real::literal>(2.0)};
-  ultra::real::literal *c3 {prob.sset.insert<ultra::real::literal>(3.0)};
-  ultra::real::literal *x {prob.sset.insert<ultra::real::literal>(x_val)};
-  ultra::real::literal *neg_x {prob.sset.insert<ultra::real::literal>(-x_val)};
-  ultra::real::literal *y {prob.sset.insert<ultra::real::literal>(y_val)};
-  ultra::nullary *z {prob.sset.insert<Z>()};
+  ultra::real::literal *c0 {prob.insert<ultra::real::literal>(0.0)};
+  ultra::real::literal *c1 {prob.insert<ultra::real::literal>(1.0)};
+  ultra::real::literal *c2 {prob.insert<ultra::real::literal>(2.0)};
+  ultra::real::literal *c3 {prob.insert<ultra::real::literal>(3.0)};
+  ultra::real::literal *x {prob.insert<ultra::real::literal>(x_val)};
+  ultra::real::literal *neg_x {prob.insert<ultra::real::literal>(-x_val)};
+  ultra::real::literal *y {prob.insert<ultra::real::literal>(y_val)};
+  ultra::nullary *z {prob.insert<Z>()};
 
-  ultra::function *f_abs {prob.sset.insert<ultra::real::abs>()};
-  ultra::function *f_add {prob.sset.insert<ultra::real::add>()};
-  ultra::function *f_aq {prob.sset.insert<ultra::real::aq>()};
-  ultra::function *f_cos {prob.sset.insert<ultra::real::cos>()};
-  ultra::function *f_div {prob.sset.insert<ultra::real::div>()};
-  ultra::function *f_idiv {prob.sset.insert<ultra::real::idiv>()};
-  ultra::function *f_ife {prob.sset.insert<ultra::real::ife>()};
-  ultra::function *f_ifz {prob.sset.insert<ultra::real::ifz>()};
-  ultra::function *f_ln {prob.sset.insert<ultra::real::ln>()};
-  ultra::function *f_max {prob.sset.insert<ultra::real::max>()};
-  ultra::function *f_mul {prob.sset.insert<ultra::real::mul>()};
-  ultra::function *f_sigmoid {prob.sset.insert<ultra::real::sigmoid>()};
-  ultra::function *f_sin {prob.sset.insert<ultra::real::sin>()};
-  ultra::function *f_sqrt {prob.sset.insert<ultra::real::sqrt>()};
-  ultra::function *f_sub {prob.sset.insert<ultra::real::sub>()};
+  ultra::function *f_abs {prob.insert<ultra::real::abs>()};
+  ultra::function *f_add {prob.insert<ultra::real::add>()};
+  ultra::function *f_aq {prob.insert<ultra::real::aq>()};
+  ultra::function *f_cos {prob.insert<ultra::real::cos>()};
+  ultra::function *f_div {prob.insert<ultra::real::div>()};
+  ultra::function *f_idiv {prob.insert<ultra::real::idiv>()};
+  ultra::function *f_ife {prob.insert<ultra::real::ife>()};
+  ultra::function *f_ifz {prob.insert<ultra::real::ifz>()};
+  ultra::function *f_ln {prob.insert<ultra::real::ln>()};
+  ultra::function *f_max {prob.insert<ultra::real::max>()};
+  ultra::function *f_mul {prob.insert<ultra::real::mul>()};
+  ultra::function *f_sigmoid {prob.insert<ultra::real::sigmoid>()};
+  ultra::function *f_sin {prob.insert<ultra::real::sin>()};
+  ultra::function *f_sqrt {prob.insert<ultra::real::sqrt>()};
+  ultra::function *f_sub {prob.insert<ultra::real::sub>()};
 
-  ultra::str::literal *s1 {prob.sset.insert<ultra::str::literal>("hello", 1)};
-  ultra::str::literal *s2 {prob.sset.insert<ultra::str::literal>("world", 1)};
-  ultra::str::literal *s3 {prob.sset.insert<ultra::str::literal>(":-)", 1)};
+  ultra::str::literal *s1 {prob.insert<ultra::str::literal>("hello", 1)};
+  ultra::str::literal *s2 {prob.insert<ultra::str::literal>("world", 1)};
+  ultra::str::literal *s3 {prob.insert<ultra::str::literal>(":-)", 1)};
 
-  ultra::function *s_ife {prob.sset.insert<ultra::str::ife>(
+  ultra::function *s_ife {prob.insert<ultra::str::ife>(
       0, ultra::function::param_data_types{1, 1, 0, 0})};
-  ultra::function *f_len {prob.sset.insert<ultra::real::length>(
+  ultra::function *f_len {prob.insert<ultra::real::length>(
       0, ultra::function::param_data_types{1})};
 };
 
