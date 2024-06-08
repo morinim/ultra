bf287db6901d25300e688619ee74ec79bffa2eba
Manlio Morini <morinim@gmail.com>
2024-06-08 09:44:33 +0200

    feat: add thread-safe queue

---
+++ src/utility/ts_queue.h
@@ -0,0 +1,135 @@
+/**
+ *  \file
+ *  \remark This file is part of ULTRA.
+ *
+ *  \copyright Copyright (C) 2024 EOS di Manlio Morini.
+ *
+ *  \license
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ *  You can obtain one at http://mozilla.org/MPL/2.0/
+ */
+
+#if !defined(ULTRA_TS_QUEUE_H)
+#define      ULTRA_TS_QUEUE_H
+
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+
+///
+/// Allows multiple threads to aenqueue and dequeue elements concurrently.
+///
+///
+///
+template <class T>
+class ts_queue
+{
+public:
+  using container_type = std::queue<T>;
+  using value_type = container_type::value_type;
+  using size_type = container_type::size_type;
+  using reference = container_type::reference;
+  using const_reference = container_type::const_reference;
+
+  ts_queue(const ts_queue &) = delete;
+  ts_queue &operator=(const ts_queue &) = delete;
+
+  /// Pushes the given element to the end of the queue.
+  ///
+  /// \\param[in] value the value of the element to push
+  void push(const T &item)
+  {
+    {
+      std::lock_guard lock(mutex_);
+      queue_.push(item);
+    }
+
+    cond_.notify_one();
+  }
+
+  /// Pushes a new element to the end of the queue.
+  ///
+  /// \param[in] args arguments to forward to the constructor of the element
+  ///
+  /// The element is constructed in-place, i.e. no copy or move operations are
+  /// performed. The constructor of the element is called with exactly the same
+  /// arguments as supplied to the function.
+  template<class... Args>
+  void emplace(Args &&...args)
+  {
+    {
+      std::lock_guard lock(mutex_);
+      queue_.emplace(std::forward<Args>(args)...);
+    }
+
+    cond_.notify_one();
+  }
+
+  /// Removes an element from the front of the queue.
+  ///
+  /// \return the first element
+  ///
+  /// \warning
+  /// Blocks if queue is empty.
+  T pop()
+  {
+    // `condition_variable` works only with `unique_lock`.
+    std::unique_lock lock(mutex_);
+
+    // Wait until queue is not empty.
+    cond_.wait(lock, [this] { return !queue_.empty(); });
+
+    // Retrieve item.
+    const T item(std::move(queue_.front()));
+    queue_.pop();
+
+    return item;
+  }
+
+  /// Removes an element from the front of the queue.
+  ///
+  /// \return the first element if available, otherwise an empty object
+  ///
+  /// \remark
+  /// Doesn't block if queue is empty.
+  [[nodiscard]] std::optional<T> try_pop()
+  {
+    std::lock_guard lock(mutex_);
+
+    if (queue_.empty())
+      return {};
+
+    // Retrieve item.
+    const T item(std::move(queue_.front()));
+    queue_.pop();
+    return item;
+  }
+
+  /// Checks if the container has no elements.
+  ///
+  /// \return `true` if the container is empty, `false` otherwise
+  ///
+  /// \warning
+  /// Should only be used in a multiple producer single consumer environment.
+  /// In general the queue cannot guarantee that matters won't change between
+  /// the time the client queries `empty()` and the time `pop()` is called,
+  /// making the point entirely moot, and this code a potential source of
+  /// intermittent (read: hard to pin) bugs:
+  /// ```c++
+  /// if (!queue.empty())
+  /// {
+  ///   // What could possibly go wrong? A lot, it turns out.
+  ///   auto elem = queue.pop();
+  /// }
+  [[nodiscard]] bool empty() const
+  {
+    std::lock_guard lock(mutex_);
+    return queue_.empty();
+  }
+
+private:
+  std::queue<T> queue_ {};           // underlying queue
+  mutable std::mutex mutex_ {};      // for thread synchronization
+  std::condition_variable cond_ {};  // for signaling
+};  // class ts_queue
