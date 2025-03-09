5125e3986b90d51c09fde649dd2b9c0212d53463
Manlio Morini <morinim@gmail.com>
2025-03-09 16:55:08 +0100

    test: add 8 queens puzzle as test suite

--- src/test/hga_search.cc
+++ src/test/hga_search.cc
@@ -27,8 +27,6 @@ TEST_CASE("String guess")
 {
   using namespace ultra;
 
-  log::reporting_level = log::lWARNING;
-
   const std::string target = "Hello World";
   const std::string CHARSET =
     " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!";
@@ -55,6 +53,45 @@ TEST_CASE("String guess")
   const auto res(search.run(10));
 
   CHECK(res.best_measurements.fitness == doctest::Approx(target.length()));
+}
+
+TEST_CASE("8 Queens")
+{
+  using namespace ultra;
+
+  const int NQUEENS(10);
+
+  hga::problem prob;
+  prob.insert<hga::permutation>(NQUEENS);
+
+  // Fitness function.
+  auto f = [](const hga::individual &x)
+  {
+    double attacks(0);
+
+    const auto columns(std::get<D_IVECTOR>(x[0]));
+
+    for (int queen(0); queen < NQUEENS - 1; ++queen)  // skips the last queen
+    {
+      const int row(columns[queen]);
+
+      for (int i(queen + 1); i < NQUEENS; ++i)
+      {
+        const int other_row(columns[i]);
+
+        if (std::abs(other_row - row) == i - queen)  // diagonal
+          ++attacks;
+      }
+    }
+
+    return -attacks;
+  };
+
+  // Let's go.
+  hga::search search(prob, f);
+  const auto res(search.run(5));
+
+  CHECK(res.best_measurements.fitness == doctest::Approx(0.0));
 }
 
 }  // TEST_SUITE("HGA::SEARCH")
