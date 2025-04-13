82c5d924fbfd513d7f95fdc678f78ffe0c7cb488
Manlio Morini <morinim@gmail.com>
2025-04-13 10:17:20 +0200

    test: add speed test comparison for single-task scenario

--- src/test/speed_thread_pool.cc
+++ src/test/speed_thread_pool.cc
@@ -23,55 +23,64 @@ void task()
   std::this_thread::sleep_for(0.1s);
 }
 
+void async_based(unsigned THREADS, unsigned GENERATIONS)
+{
+  const auto task_completed(
+    [](const auto &future)
+    {
+      return future.wait_for(0ms) == std::future_status::ready;
+    });
+
+  std::cout << "Starting async simulation...";
+  ultra::timer t;
+  for (unsigned n(GENERATIONS); n; --n)
+  {
+    std::vector<std::future<void>> tasks;
+
+    for (unsigned i(0); i < THREADS; ++i)
+      tasks.push_back(std::async(std::launch::async, task));
+
+    while (!std::ranges::all_of(tasks, task_completed))
+      std::this_thread::sleep_for(50ms);
+  }
+
+  const auto e_ms(t.elapsed().count());
+  std::cout << " test finished. Time elapsed: " << e_ms << "ms\n";
+}
+
+void thread_pool_based(unsigned THREADS, unsigned GENERATIONS)
+{
+  ultra::thread_pool tp(THREADS);
+
+  std::cout << "Starting thread pool simulation... ";
+  ultra::timer t;
+  for (unsigned n(GENERATIONS); n; --n)
+  {
+    for (unsigned i(0); i < THREADS; ++i)
+      tp.execute(task);
+
+    tp.wait();
+  }
+
+  const auto e_ms(t.elapsed().count());
+  std::cout << " test finished. Time elapsed: " << e_ms << "ms\n";
+}
+
 int main()
 {
-  using namespace ultra;
+  const unsigned GENERATIONS(100);
+
+  std::cout << "SINGLE THREAD\n";
+  {
+    async_based(1, GENERATIONS);
+    thread_pool_based(1, GENERATIONS);
+  }
 
   const auto THREADS(
     std::max<unsigned>(2, std::thread::hardware_concurrency()));
 
-  const unsigned GENERATIONS(100);
-
   // Async-based execution.
-  {
-    const auto task_completed(
-      [](const auto &future)
-      {
-        return future.wait_for(0ms) == std::future_status::ready;
-      });
-
-    std::cout << "Starting async-based simulation\n";
-    ultra::timer t;
-    for (unsigned n(GENERATIONS); n; --n)
-    {
-      std::vector<std::future<void>> tasks;
-
-      for (unsigned i(0); i < THREADS; ++i)
-        tasks.push_back(std::async(std::launch::async, task));
-
-      while (!std::ranges::all_of(tasks, task_completed))
-        std::this_thread::sleep_for(100ms);
-    }
-
-    const auto e_ms(t.elapsed().count());
-    std::cout << "Test finished.\nTime elapsed: " << e_ms << "ms\n";
-  }
-
-  // Thread pool-based execution.
-  {
-    thread_pool tp(THREADS);
-
-    std::cout << "Starting async-based simulation\n";
-    ultra::timer t;
-    for (unsigned n(GENERATIONS); n; --n)
-    {
-      for (unsigned i(0); i < THREADS; ++i)
-        tp.execute(task);
-
-      tp.wait();
-    }
-
-    const auto e_ms(t.elapsed().count());
-    std::cout << "Test finished.\nTime elapsed: " << e_ms << "ms\n";
-  }
+  std::cout << "\n\nMULTIPLE THREADS (" << THREADS << ")\n";
+  async_based(THREADS, GENERATIONS);
+  thread_pool_based(THREADS, GENERATIONS);
 }
