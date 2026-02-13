151e454958b46f0807d24b35ae8f8c3056c4f709
Manlio Morini <info@ultraevolution.org>
2026-02-13 10:44:03 +0100

    docs: fix header comment to reflect summary XML merging

--- src/wopr/merge_summary.py
+++ src/wopr/merge_summary.py
@@ -7,7 +7,11 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this file,
 #  You can obtain one at http://mozilla.org/MPL/2.0/
 #
-#  A command line utility to simplify xrff preprocessing.
+#  A command line utility to merge ULTRA summary XML files.
+#
+#  It combines run counts, elapsed time, success rate, and pooled fitness
+#  statistics, and rewrites best/solutions run indices with the proper offset.
+#  The output is re-signed with a CRC32 checksum.
 #
 #  \see https://ultraevolution.org/blog/combine_test_batches/
 #
