41273c1b2c599e73923293fd103cbc3beb462881
Manlio Morini <info@ultraevolution.org>
2026-06-23 16:11:39 +0200

    chore: improve Flycheck setup for template sources
    Add third-party include paths, libc++ arguments, and header pre-includes for
    .tcc files so Clang can check template implementations in context.

--- misc/.dir-locals.el
+++ misc/.dir-locals.el
@@ -6,14 +6,50 @@
   . ((flycheck-checker . c/c++-clang)
      (flycheck-clang-language-standard . "c++23")
      (eval
-      . (let ((root (locate-dominating-file buffer-file-name "src")))
+      . (let* ((root (locate-dominating-file buffer-file-name "src"))
+               (src-root (expand-file-name "src/" root)))
           (setq-local flycheck-clang-include-path
-                      (list (expand-file-name "src" root)))))))
+                      (list (expand-file-name "src" root)
+                            (expand-file-name "src/third_party" root)))
+          (setq-local flycheck-clang-args '("-stdlib=libc++"))
+          (when (and buffer-file-name (string-match-p "\\.tcc\\'" buffer-file-name))
+            (let* ((relative-file (file-relative-name buffer-file-name src-root))
+                   (header-map
+                    '(("kernel/layered_population_iterator.tcc" . "kernel/layered_population.h")
+                      ("kernel/gp/individual_exon_view.tcc" . "kernel/gp/individual.h")
+                      ("kernel/gp/individual_iterator.tcc" . "kernel/gp/individual.h")
+                      ("kernel/gp/src/oracle_internal.tcc" . "kernel/gp/src/oracle.h")
+                      ("kernel/gp/src/evaluator_internal.tcc" . "kernel/gp/src/evaluator.h")))
+                   (mapped-header (cdr (assoc relative-file header-map)))
+                   (header-file (if mapped-header
+                                    (expand-file-name mapped-header src-root)
+                                  (replace-regexp-in-string "\\.tcc\\'" ".h" buffer-file-name))))
+              (when (file-exists-p header-file)
+                (setq-local flycheck-clang-args
+                            (append flycheck-clang-args (list "-include" header-file))))))))))
 
  (c++-ts-mode
   . ((flycheck-checker . c/c++-clang)
      (flycheck-clang-language-standard . "c++23")
      (eval
-      . (let ((root (locate-dominating-file buffer-file-name "src")))
+      . (let* ((root (locate-dominating-file buffer-file-name "src"))
+               (src-root (expand-file-name "src/" root)))
           (setq-local flycheck-clang-include-path
-                      (list (expand-file-name "src" root))))))))
+                      (list (expand-file-name "src" root)
+                            (expand-file-name "src/third_party" root)))
+          (setq-local flycheck-clang-args '("-stdlib=libc++"))
+          (when (and buffer-file-name (string-match-p "\\.tcc\\'" buffer-file-name))
+            (let* ((relative-file (file-relative-name buffer-file-name src-root))
+                   (header-map
+                    '(("kernel/layered_population_iterator.tcc" . "kernel/layered_population.h")
+                      ("kernel/gp/individual_exon_view.tcc" . "kernel/gp/individual.h")
+                      ("kernel/gp/individual_iterator.tcc" . "kernel/gp/individual.h")
+                      ("kernel/gp/src/oracle_internal.tcc" . "kernel/gp/src/oracle.h")
+                      ("kernel/gp/src/evaluator_internal.tcc" . "kernel/gp/src/evaluator.h")))
+                   (mapped-header (cdr (assoc relative-file header-map)))
+                   (header-file (if mapped-header
+                                    (expand-file-name mapped-header src-root)
+                                  (replace-regexp-in-string "\\.tcc\\'" ".h" buffer-file-name))))
+              (when (file-exists-p header-file)
+                (setq-local flycheck-clang-args
+                            (append flycheck-clang-args (list "-include" header-file)))))))))))
